6c1ee72aced066ff27837fbb6068d7f85a125700
Jovi Hsu <jv.hsu@outlook.com>
2021-11-01 21:36:38 +0800

    a minimal yew framework with npm

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+mod app;
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn run_app() -> Result<(), JsValue> {
+    yew::start_app::<app::App>();
+
+    Ok(())
+}
---
+++ www/index.js
@@ -0,0 +1,5 @@
+import { run_app } from '../pkg/webgateway_bg.js';
+async function main() {
+    run_app();
+}
+main()
