da807ead3ef2d57073f92844985e7f5cb52e04a3
Jovi Hsu <jv.hsu@outlook.com>
2021-11-29 08:17:39 +0800

    rename

--- frontend/src/pages/page_home.rs
+++ frontend/src/pages/page_home.rs
@@ -14,7 +14,7 @@ impl Component for PageHome {
 
     fn view(&self) -> Html {
         html! {
-            <crate::pages::page_remote::PageRemote/>
+            <crate::pages::page_vnc::PageVnc/>
         }
     }
 
