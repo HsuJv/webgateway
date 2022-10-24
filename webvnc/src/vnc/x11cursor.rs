a031a9d0472677cb17cc269abdd1cbc7349582bc
Jovi Hsu <jv.hsu@outlook.com>
2022-10-24 08:49:02 +0000

    fix x11 mouse get wrong representation

--- webvnc/src/vnc/x11cursor.rs
+++ webvnc/src/vnc/x11cursor.rs
@@ -1,16 +1,12 @@
 use super::MouseEventType;
 
 pub struct MouseUtils {
-    down: bool,
     mask: u8,
 }
 
 impl MouseUtils {
     pub fn new() -> Self {
-        Self {
-            down: false,
-            mask: 0,
-        }
+        Self { mask: 0 }
     }
     pub fn get_mouse_sym(
         &mut self,
@@ -19,16 +15,14 @@ impl MouseUtils {
     ) -> (u16, u16, u8) {
         let x: u16 = event.offset_x().try_into().unwrap_or(0);
         let y: u16 = event.offset_y().try_into().unwrap_or(0);
-        let mask: u8 = (event.button() << 1).try_into().unwrap_or(0);
+        let mask: u8 = (1 << event.button()).try_into().unwrap_or(0);
 
         match et {
             MouseEventType::Down => {
-                self.down = true;
-                self.mask = self.down as u8 | mask;
+                self.mask |= mask;
             }
             MouseEventType::Up => {
-                self.down = false;
-                self.mask = self.down as u8 & (!mask);
+                self.mask &= !mask;
             }
 
             MouseEventType::Move => {}
