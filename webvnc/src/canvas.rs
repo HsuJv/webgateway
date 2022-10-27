f6861381bf70dbde65caf13f4eb7686fd037dcd3
Jovi Hsu <jv.hsu@outlook.com>
2022-10-27 08:56:44 +0000

    Use vnc-rs lib rather than impl it in the app

--- webvnc/src/canvas.rs
+++ webvnc/src/canvas.rs
@@ -1,22 +1,37 @@
-use std::rc::Rc;
-
+// use crate::input::{X11Event, KeyEventType, MouseEventType};
+// use rdp::core::event::BitmapEvent;
 use crate::{
-    console_log, log,
-    vnc::{ImageData, ImageType, MouseEventType, Vnc},
+    x11cursor::MouseUtils,
+    x11keyboard::{self, KeyboardUtils},
 };
+use std::{
+    cell::{Cell, RefCell},
+    rc::Rc,
+    sync::{Arc, Mutex},
+};
+use tokio::sync::mpsc;
+use vnc::{Rect, X11Event};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::{Clamped, JsCast};
+use wasm_timer::Instant;
 use web_sys::{
-    CanvasRenderingContext2d, HtmlButtonElement, HtmlCanvasElement, HtmlImageElement,
-    KeyboardEvent, MouseEvent,
+    CanvasRenderingContext2d, HtmlButtonElement, HtmlCanvasElement, KeyboardEvent, MouseEvent,
 };
+
+const MILLIS_IN_SEC: u32 = 1000;
+
 struct Canvas {
     canvas: HtmlCanvasElement,
     ctx: CanvasRenderingContext2d,
+    output: mpsc::Sender<X11Event>,
+    refresh_interval: u32,
+    video_mem: RefCell<Vec<u8>>,
+    timer: Cell<Instant>,
+    resolution: Cell<(u32, u32)>,
 }
 
 impl Canvas {
-    fn new() -> Self {
+    fn new(sender: mpsc::Sender<X11Event>, refresh_rate: u16) -> Self {
         let document = web_sys::window().unwrap().document().unwrap();
         let canvas = document.get_element_by_id("vnc-canvas").unwrap();
         let canvas: HtmlCanvasElement = canvas
@@ -29,23 +44,40 @@ impl Canvas {
             .unwrap()
             .dyn_into::<CanvasRenderingContext2d>()
             .unwrap();
-        Self { canvas, ctx }
+        Self {
+            canvas,
+            ctx,
+            output: sender,
+            refresh_interval: MILLIS_IN_SEC / refresh_rate as u32,
+            video_mem: RefCell::new(Vec::new()),
+            timer: Cell::new(Instant::now()),
+            resolution: Cell::new((0, 0)),
+        }
     }
 
     fn set_resolution(&self, width: u32, height: u32) {
         // set hight & width
         self.canvas.set_height(height);
         self.canvas.set_width(width);
+        self.video_mem
+            .borrow_mut()
+            .resize(height as usize * width as usize * 4, 0xff);
         self.ctx.rect(0_f64, 0_f64, width as f64, height as f64);
+        self.resolution.set((width, height));
         self.ctx.fill();
     }
 
-    fn bind(&self, vnc: &Vnc) {
-        let handler = vnc.clone();
+    fn bind(&self) {
+        let sender = self.output.clone();
         let key_down = move |e: KeyboardEvent| {
+            let sender = sender.clone();
             e.prevent_default();
             e.stop_propagation();
-            handler.key_press(e, true);
+            futures::executor::block_on(async move {
+                let _ = sender
+                    .send(X11Event::KeyEvent(KeyboardUtils::get_keysym(e), true))
+                    .await;
+            });
         };
 
         let handler = Box::new(key_down) as Box<dyn FnMut(_)>;
@@ -57,11 +89,16 @@ impl Canvas {
             .unwrap();
         cb.forget();
 
-        let handler = vnc.clone();
+        let sender = self.output.clone();
         let key_up = move |e: KeyboardEvent| {
+            let sender = sender.clone();
             e.prevent_default();
             e.stop_propagation();
-            handler.key_press(e, false);
+            futures::executor::block_on(async move {
+                let _ = sender
+                    .send(X11Event::KeyEvent(KeyboardUtils::get_keysym(e), false))
+                    .await;
+            });
         };
 
         let handler = Box::new(key_up) as Box<dyn FnMut(_)>;
@@ -73,7 +110,7 @@ impl Canvas {
             .unwrap();
         cb.forget();
 
-        let handler = vnc.clone();
+        let sender = self.output.clone();
         let ctrl_alt_del_btn = web_sys::window()
             .unwrap()
             .document()
@@ -84,7 +121,27 @@ impl Canvas {
             .map_err(|_| ())
             .unwrap();
         let ctrl_alt_del = move || {
-            handler.ctrl_alt_del();
+            let sender = sender.clone();
+            futures::executor::block_on(async move {
+                let _ = sender
+                    .send(X11Event::KeyEvent(x11keyboard::XK_Control_L, true))
+                    .await;
+                let _ = sender
+                    .send(X11Event::KeyEvent(x11keyboard::XK_Alt_L, true))
+                    .await;
+                let _ = sender
+                    .send(X11Event::KeyEvent(x11keyboard::XK_Delete, true))
+                    .await;
+                let _ = sender
+                    .send(X11Event::KeyEvent(x11keyboard::XK_Delete, false))
+                    .await;
+                let _ = sender
+                    .send(X11Event::KeyEvent(x11keyboard::XK_Alt_L, false))
+                    .await;
+                let _ = sender
+                    .send(X11Event::KeyEvent(x11keyboard::XK_Control_L, false))
+                    .await;
+            });
         };
         let handler = Box::new(ctrl_alt_del) as Box<dyn FnMut()>;
 
@@ -101,10 +158,15 @@ impl Canvas {
 
         // to do:
         // calculate relation position
-        let handler = vnc.clone();
+        let sender = self.output.clone();
         let mouse_move = move |e: MouseEvent| {
+            let sender = sender.clone();
+            e.prevent_default();
             e.stop_propagation();
-            handler.mouse_event(e, MouseEventType::Move);
+            let (x, y, mask) = MouseUtils::get_mouse_sym(e);
+            futures::executor::block_on(async move {
+                let _ = sender.send(X11Event::PointerEvent(x, y, mask)).await;
+            });
         };
 
         let handler = Box::new(mouse_move) as Box<dyn FnMut(_)>;
@@ -116,10 +178,15 @@ impl Canvas {
             .unwrap();
         cb.forget();
 
-        let handler = vnc.clone();
+        let sender = self.output.clone();
         let mouse_down = move |e: MouseEvent| {
+            let sender = sender.clone();
+            // e.prevent_default();
             e.stop_propagation();
-            handler.mouse_event(e, MouseEventType::Down);
+            let (x, y, mask) = MouseUtils::get_mouse_sym(e);
+            futures::executor::block_on(async move {
+                let _ = sender.send(X11Event::PointerEvent(x, y, mask)).await;
+            });
         };
 
         let handler = Box::new(mouse_down) as Box<dyn FnMut(_)>;
@@ -131,10 +198,15 @@ impl Canvas {
             .unwrap();
         cb.forget();
 
-        let handler = vnc.clone();
+        let sender = self.output.clone();
         let mouse_up = move |e: MouseEvent| {
+            let sender = sender.clone();
+            e.prevent_default();
             e.stop_propagation();
-            handler.mouse_event(e, MouseEventType::Up);
+            let (x, y, mask) = MouseUtils::get_mouse_sym(e);
+            futures::executor::block_on(async move {
+                let _ = sender.send(X11Event::PointerEvent(x, y, mask)).await;
+            });
         };
 
         let handler = Box::new(mouse_up) as Box<dyn FnMut(_)>;
@@ -159,70 +231,55 @@ impl Canvas {
             .add_event_listener_with_callback("contextmenu", cb.as_ref().unchecked_ref())
             .unwrap();
         cb.forget();
+
+        // initilize the timer
+        self.timer.set(Instant::now());
     }
 
-    fn draw(&self, ri: &ImageData) {
-        match ri.type_ {
-            ImageType::Copy => {
-                //copy
-                let sx = (ri.data[0] as u16) << 8 | ri.data[1] as u16;
-                let sy = (ri.data[2] as u16) << 8 | ri.data[3] as u16;
+    fn draw(&self, rect: Rect, mut data: Vec<u8>) {
+        // let mut y = 0;
+        // let mut x;
 
-                let _ = self
-                    .ctx
-                    .draw_image_with_html_canvas_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-                        &self.canvas,
-                        sx as f64,
-                        sy as f64,
-                        ri.width as f64,
-                        ri.height as f64,
-                        ri.x as f64,
-                        ri.y as f64,
-                        ri.width as f64,
-                        ri.height as f64,
-                    );
-            }
-            ImageType::Fill => {
-                // fill
-                let (r, g, b) = (ri.data[2], ri.data[1], ri.data[0]);
-                let style = format!("rgb({},{},{})", r, g, b);
-                self.ctx.set_fill_style(&JsValue::from_str(&style));
-            }
-            ImageType::Raw => {
-                let data = web_sys::ImageData::new_with_u8_clamped_array_and_sh(
-                    Clamped(&ri.data),
-                    ri.width as u32,
-                    ri.height as u32,
-                );
-                if data.is_err() {
-                    console_log!(
-                        "renderring failed at ({}, {}), width {}, height {}, len {}",
-                        ri.x,
-                        ri.y,
-                        ri.width,
-                        ri.height,
-                        ri.data.len(),
-                    );
-                }
-                let data = data.unwrap();
-                let _ = self.ctx.put_image_data(&data, ri.x as f64, ri.y as f64);
-            }
-            ImageType::Jpeg => {
-                let image = HtmlImageElement::new().unwrap();
-                let base64 = crate::utils::base64_encode(&ri.data);
-                image.set_src(&format!(
-                    "data:image/jpeg;base64,{}",
-                    std::str::from_utf8(&base64).unwrap()
-                ));
-                let _ = self.ctx.draw_image_with_html_image_element_and_dw_and_dh(
-                    &image,
-                    ri.x as f64,
-                    ri.y as f64,
-                    ri.width as f64,
-                    ri.height as f64,
-                );
-            }
+        // only update the vedio buffer
+        // let mut video = self.video_mem.borrow_mut();
+        // while y < rect.height {
+        //     x = 0;
+        //     let mut idx = (y as usize * rect.width as usize) * 4;
+        //     let mut d_idx =
+        //         ((y + rect.y) as usize * self.resolution.get().0 as usize + rect.x as usize) * 4;
+        //     while x < rect.width {
+        //         video[d_idx] = data[idx];
+        //         video[d_idx + 1] = data[idx + 1];
+        //         video[d_idx + 2] = data[idx + 2];
+        //         idx += 4;
+        //         d_idx += 4;
+        //         x += 1;
+        //     }
+        //     y += 1;
+        // }
+
+        // if self.timer.get().elapsed().as_millis() < self.refresh_interval as u128 {
+        //     // if the time elapsed has not exceeded the refresh_interval
+        //     // return to decrease the calling of render
+        //     return;
+        // } else {
+        //     self.timer.set(Instant::now());
+        // }
+
+        let mut idx = 3;
+        while idx < data.len() {
+            data[idx] = 255;
+            idx += 4;
         }
+
+        let data = web_sys::ImageData::new_with_u8_clamped_array_and_sh(
+            Clamped(&data),
+            rect.width as u32,
+            rect.height as u32,
+        );
+
+        let data = data.unwrap();
+        let _ = self.ctx.put_image_data(&data, rect.x as f64, rect.y as f64);
     }
 
     fn close(&self) {
@@ -243,22 +300,19 @@ impl Clone for CanvasUtils {
 }
 
 impl CanvasUtils {
-    pub fn new() -> Self {
+    pub fn new(sender: mpsc::Sender<X11Event>, refresh_rate: u16) -> Self {
         Self {
-            inner: Rc::new(Canvas::new()),
+            inner: Rc::new(Canvas::new(sender, refresh_rate)),
         }
     }
 
     pub fn init(&self, width: u32, height: u32) {
         self.inner.as_ref().set_resolution(width, height);
+        self.inner.as_ref().bind();
     }
 
-    pub fn bind(&self, vnc: &Vnc) {
-        self.inner.as_ref().bind(vnc);
-    }
-
-    pub fn draw(&self, ri: &ImageData) {
-        self.inner.as_ref().draw(ri);
+    pub fn draw(&self, rect: Rect, data: Vec<u8>) {
+        self.inner.as_ref().draw(rect, data);
     }
 
     pub fn close(&self) {
---
+++ webvnc/src/x11cursor.rs
@@ -0,0 +1,31 @@
+use tracing::info;
+
+pub struct MouseUtils;
+
+impl MouseUtils {
+    pub fn get_mouse_sym(event: web_sys::MouseEvent) -> (u16, u16, u8) {
+        let x: u16 = event.offset_x().try_into().unwrap_or(0);
+        let y: u16 = event.offset_y().try_into().unwrap_or(0);
+        let buttons = event.buttons();
+
+        // On a conventional mouse, buttons 1, 2, and 3 correspond to the left,
+        // middle, and right buttons on the mouse.  On a wheel mouse, each step
+        // of the wheel upwards is represented by a press and release of button
+        // 4, and each step downwards is represented by a press and release of
+        // button 5.
+        let mut mask = 0;
+        let left = buttons & 0x1 > 0;
+        let right = buttons & 0x2 > 0;
+        let middle = buttons & 0x4 > 0;
+        if left {
+            mask |= 1;
+        }
+        if middle {
+            mask |= 1 << 1;
+        }
+        if right {
+            mask |= 1 << 2;
+        }
+        (x, y, mask)
+    }
+}
